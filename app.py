beb64ceb6cdaa15adf422c8ab0e292fc5a355e2b
Wilson Li <wilsonli890409@gmail.com>
2020-07-05 00:59:49 +0800

    fixed stickers

--- app.py
+++ app.py
@@ -41,13 +41,13 @@ def handle_message(event):
     r = "Wilson doesn't understand what you are saying"
     if "嗨" or '哈囉' or '你好' or 'hi' or 'hello' in msg:
         r = 'Hello'
-    if '愛' in msg:
+    elif '愛' in msg:
         r = '啾啾啾啾啾'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
 
-    if '帥' in msg:
+    elif '帥' in msg:
         sticker_message = StickerSendMessage(
             package_id='1',
             sticker_id='5'
