fc69adb8db7c78744d87844a32253b4e746c8002
Wilson Li <wilsonli890409@gmail.com>
2020-07-05 00:35:45 +0800

    fixed stickers

--- app.py
+++ app.py
@@ -43,62 +43,18 @@ def handle_message(event):
         r = 'Hello'
     if '愛' in msg:
         r = '啾啾啾啾啾'
-    if '帥' in msg:
-        sticker_message = StickerSendMessage(
-        package_id='23',
-        sticker_id='2'
-)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
 
+    if '帥' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='5'
+)
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
 
 if __name__ == "__main__":
-=======
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi('dvi4+TbHBY3peCO29ldcYeJjRoddEKKrkiX8uWJTSsXITf/e+sTMM/g5MkQ8WNITA0/XgT9j6Kgwx0/sKMEN9azOqD/FBsfRuPYAHrTkeYJtf99O23x7bJtoMiy99y8Ac/Mhl3QqJnbEanuMNI/RLwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('4cf41939cf6e66179069b061b37c4ec6')
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
-if __name__ == "__main__":
->>>>>>> ca9c51d658879fa3e0a3947f3e07db091ae57136
     app.run()
