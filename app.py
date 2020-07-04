9337c164e69545fac3406df1eefbc1175fec01b2
Wilson Li <wilsonli890409@gmail.com>
2020-07-05 01:05:55 +0800

    fixed sticker problem

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def handle_message(event):
         sticker_message = StickerSendMessage(
             package_id='1',
             sticker_id='5'
-)
+        )
         line_bot_api.reply_message(
             event.reply_token,
             sticker_message)
