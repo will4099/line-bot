5ad49baf368809877413969c8db8d00578a6b61e
Wilson Li <wilsonli890409@gmail.com>
2020-07-05 15:49:29 +0800

    fixed reply

--- app.py
+++ app.py
@@ -53,12 +53,19 @@ def handle_message(event):
 
     if '嗨' or '哈囉' or '你好' or 'hi' or 'hello' in msg:
         r = '嗨'
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
     elif '愛' in msg:
         r = '啾啾啾啾啾'
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=r))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=r))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=r))
     
 
 if __name__ == "__main__":
