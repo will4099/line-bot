73a97eec3342ca2b82bb25a55a143b548f0bf99c
Wilson Li <wilsonli890409@gmail.com>
2020-07-05 16:32:06 +0800

    fixed reply

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r = '黎黎不懂'
     if '帥' in msg :
         sticker_message = StickerSendMessage(
             package_id='1',
@@ -49,10 +50,9 @@ def handle_message(event):
             sticker_message)
     if '嗨' or '哈囉' or '你好' or 'hi' or 'hello' in msg :
         r = '嗨'
-    if '愛' in msg :
+    if '愛' or '啾' in msg :
         r = '啾啾啾啾啾'
 
-    r = '黎黎不懂'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
