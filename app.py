d87e2d142747496d7fc457316458f260417b15dc
Wilson Li <wilsonli890409@gmail.com>
2020-07-08 17:23:45 +0800

    fixed reply

--- app.py
+++ app.py
@@ -47,10 +47,6 @@ def handle_message(event):
         r = '啾啾啾啾啾'
     elif '想你' in msg:
         r = '帥大也是，好想好想小Q喔'
-        
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=r))
 
     if '帥' in msg:
         sticker_message = StickerSendMessage(
@@ -67,6 +63,11 @@ def handle_message(event):
             package_id='11539',
             sticker_id='52114111'
         )
+
+                
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
 
     line_bot_api.reply_message(
         event.reply_token,
