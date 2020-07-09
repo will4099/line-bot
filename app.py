3e0dad566b2485f729b74fb95e13372ca107a8b6
Wilson Li <wilsonli890409@gmail.com>
2020-07-10 00:36:59 +0800

    fixed reply

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
-
+    
     if '加油' in msg:
         r = '啾啾啾，小Q加油'
         sticker_message = StickerSendMessage(
